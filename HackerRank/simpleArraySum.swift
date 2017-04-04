ee2987f6d227b5de690215037fbb1df9e5bfc4ee
Marcel324 <mchaucer324@gmail.com>
2017-04-04 17:29:39 -0400

    HackerRank

---
+++ HackerRank/simpleArraySum.swift
@@ -0,0 +1,16 @@
+// number of elements
+var n = Int(readLine()!)!
+
+// read array and map the elements to integer
+var arr = readLine()!.characters.split(" ").map{Int(String($0))!}
+
+// variable to hold the sum of the array elements
+var sum = 0
+
+for element in arr {
+    // sum the array elements
+    sum += element
+}
+
+// print the array elements
+print(sum)
