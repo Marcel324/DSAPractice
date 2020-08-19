9a5b74b9fe1dd1c8ec8c0e2befac6fb4941931e3
Marcel <mchaucer324@gmail.com>
2020-08-19 19:13:34 -0400

    Added files to git

--- LeetCode/twoSum.playground/Contents.swift
+++ LeetCode/twoSum.playground/Contents.swift
@@ -1,14 +1,18 @@
 import UIKit
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var previousIntDict = [Int:Int]()
     for (index, num) in nums.enumerated() {
-        if let secondIndex = nums.firstIndex(of: target - num) {
-            if index != secondIndex {
-                return [index, secondIndex]
-            }
+        let complement = target - num
+        if let element = previousIntDict.first(where: { (element) in
+            return element.value == complement
+        }) {
+            return [index, element.key]
         }
+        previousIntDict[index] = num
     }
     return []
 }
 
-twoSum([1,3,2], 5)
+twoSum([3, 3], 6)
+twoSum([3, 1, 8], 11)
